f387954981bc6a56b67345fe540634f2808a868a
Chris Knight <chrisk314@gmail.com>
2018-01-22 19:59:02 +0000

    Boundary and tetmesh construction now working

--- mesh_sphere_packing/boundarypslg.py
+++ mesh_sphere_packing/boundarypslg.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
 from __future__ import print_function
 
-import os
 import sys
+import os
 from pprint import pprint
 
+from meshpy import triangle, tet
 import numpy as np
 from numpy import linalg as npl
-from meshpy import triangle, tet
 
 WITH_PBC = True
 
@@ -16,51 +15,20 @@ WITH_PBC = True
 #      : of a sphere surface. This is confusing...
 
 
-def write_geomview(points, tris, fname):
-    with open(fname, 'w') as f:
-        f.write('OFF\n')
-        f.write('%d %d %d\n' % (len(points), len(tris), 0))
-        for p in points:
-            f.write('%+1.15e %+1.15e %+1.15e\n' % (p[0], p[1], p[2]))
-        for t in tris:
-            f.write('3 %d %d %d\n' % (t[0], t[1], t[2]))
-
-
-def output_boundaries_geomview(boundaries):
-    for i, (points, tris, _) in enumerate(boundaries):
-        write_geomview(points, tris, './boundary_%d.off' % i)
-
-
-def write_poly(points, edges, holes, fname):
-    with open(fname, 'w') as f:
-        f.write('%d 2 0 0\n' % len(points))
+def output_mesh_poly(points, facets, markers, holes):
+    with open('mesh.poly', 'w') as f:
+        f.write('%d 3 0 1\n' % len(points))
         for i, p in enumerate(points):
-            f.write('%d %+1.15e %+1.15e\n' % (i, p[0], p[1]))
-        f.write('%d 0\n' % len(edges))
-        for i, e in enumerate(edges):
-            f.write('%d %d %d\n' % (i, e[0], e[1]))
-        f.write('%d\n' % len(holes))
-        for i, h in enumerate(holes):
-            f.write('%d %+1.15e %+1.15e\n' % (i, h[0], h[1]))
-
-
-def output_boundaries_poly(boundaries):
-    for i, (points, edges, holes) in enumerate(boundaries):
-        write_poly(points, edges, holes, './boundary_%d.poly' % i)
-
-
-def plot_points_edges(points, edges):
-    from matplotlib import pyplot as plt
-    from matplotlib.collections import LineCollection
-    lc = LineCollection(points[edges])
-    fig = plt.figure()
-    plt.gca().add_collection(lc)
-    space = 0.05 * points.max()
-    plt.xlim(points[:,0].min() - space, points[:,0].max() + space)
-    plt.ylim(points[:,1].min() - space, points[:,1].max() + space)
-    plt.plot(points[:,0], points[:,1], 'ro')
-    fig.savefig('points_edges.png')
-    plt.show()
+            f.write('%5d %+1.15e %+1.15e %+1.15e\n' % (i, p[0], p[1], p[2]))
+        f.write('%d 1\n' % len(facets))
+        for i, (fac, m) in enumerate(zip(facets, markers)):
+            f.write('1 0 %d\n%d %d %d %d\n' % (m, 3, fac[0], fac[1], fac[2]))
+        if len(holes):
+            f.write('%d\n' % len(holes))
+            for i, h in enumerate(holes):
+                f.write('%5d %+1.15e %+1.15e %+1.15e\n' % (i, h[0], h[1], h[2]))
+        else:
+            f.write('0\n')
 
 
 def read_geomview(fname):
@@ -87,64 +55,6 @@ def load_segments_geomview(prefix):
     return [read_geomview(fname) for fname in geomfiles]
 
 
-def create_cgal_polyhedron_3(segments):
-    from CGAL.CGAL_Kernel import Point_3, Segment_3, Triangle_3, Plane_3
-    from CGAL.CGAL_Polyhedron_3 import Polyhedron_3
-    from CGAL.CGAL_AABB_tree import AABB_tree_Polyhedron_3_Facet_handle
-    s = segments[0]
-    P = Polyhedron_3()
-    cgal_points = [Point_3(*p) for p in s[0]]
-    for t in s[0]:
-        P.make_triangle(*[cgal_points[v] for v in t])
-    return P
-
-
-def tet_cube():
-    points = np.array([
-        [0.,0.,0.], [0.,1.,0.], [0.,1.,1.], [0.,0.,1.],
-        [1.,0.,0.], [1.,1.,0.], [1.,1.,1.], [1.,0.,1.]
-    ])
-    facets = np.array([
-        [0,1,2,3], [0,4,5,1], [0,3,7,4],
-        [6,2,1,5], [6,7,3,2], [6,5,4,7]
-    ])
-
-    mesh_data = tet.MeshInfo()
-    mesh_data.set_points(points)
-    mesh_data.set_facets(facets)
-
-    rad_edge = 1.4
-    min_angle = 18.
-    max_volume = None  # 0.00001
-    #options = tet.Options('pq{}/{}Y'.format(rad_edge, min_angle))
-    options = tet.Options('pq')
-
-    return tet.build(mesh_data, options=options, max_volume=max_volume)
-
-
-def tet_cube_with_tet(opt_str=None):
-    points = np.array([
-        [0.,0.,0.], [0.,1.,0.], [0.,1.,1.], [0.,0.,1.],
-        [1.,0.,0.], [1.,1.,0.], [1.,1.,1.], [1.,0.,1.],
-        [0.5,0.6,0.5],[0.5,0.4,0.5],[0.6,0.5,0.5],[0.5,0.5,0.6]
-    ])
-    facets = np.array([
-        [0,1,2,3], [0,4,5,1], [0,3,7,4],
-        [6,2,1,5], [6,7,3,2], [6,5,4,7],
-        [8,9,10], [8,11,9], [8,11,10], [9,11,10]
-    ])
-
-    mesh_data = tet.MeshInfo()
-    mesh_data.set_points(points)
-    mesh_data.set_facets(facets)
-    mesh_data.set_holes(np.array([[0.51,0.51,0.51]]))
-
-    max_volume = None  # 0.00001
-    options = tet.Options(opt_str if opt_str else 'pq')
-
-    return tet.build(mesh_data, options=options, max_volume=max_volume)
-
-
 def get_args(argv):
     """Get command line arguments
     :return: sphere center coordinates, x, y, z, sphere radius, r,
@@ -161,17 +71,34 @@ def get_args(argv):
 def build_boundary_PSLGs(segments, Lx, Ly, Lz):
     # TODO : Break up this function a bit.
 
-    # Build list of edges for segment
-    def build_edge_list(tris, points):
-        v_adj = np.zeros(2*[points.shape[0]], dtype=np.int64)
-        v_adj[tris[:,0], tris[:,1]] = v_adj[tris[:,1], tris[:,0]] = 1
-        v_adj[tris[:,1], tris[:,2]] = v_adj[tris[:,2], tris[:,1]] = 1
-        v_adj[tris[:,2], tris[:,0]] = v_adj[tris[:,0], tris[:,2]] = 1
-        return np.array(np.where(np.triu(v_adj) == 1)).T
+    def compile_points_edges(segments):
+
+        def build_edge_list(tris, points):
+            v_adj = np.zeros(2*[points.shape[0]], dtype=np.int64)
+            v_adj[tris[:,0], tris[:,1]] = v_adj[tris[:,1], tris[:,0]] = 1
+            v_adj[tris[:,1], tris[:,2]] = v_adj[tris[:,2], tris[:,1]] = 1
+            v_adj[tris[:,2], tris[:,0]] = v_adj[tris[:,0], tris[:,2]] = 1
+            return np.array(np.where(np.triu(v_adj) == 1)).T
+
+        vcount = 0
+        all_points = []
+        all_edges = []
+        for points, tris in segments:
+            edges = build_edge_list(tris, points)
+            edges += vcount
+            vcount += len(points)
+            all_points.append(points)
+            all_edges.append(edges)
+        return np.vstack(all_points), np.vstack(all_edges)
 
     def refined_perimeter(perim, axis):
-        # TODO : remove after development
-        return perim
+
+        def filter_colocated_points(perim, axis):
+            delta = np.diff(perim[:,axis])
+            keep_idx = np.hstack(([0], np.where(~np.isclose(delta,0.))[0] + 1))
+            return perim[keep_idx]
+
+        perim = filter_colocated_points(perim, axis)
         refined_points = [perim[0]]
         for e in [[i, i+1] for i in range(perim.shape[0]-1)]:
             e_len = perim[e[1], axis] - perim[e[0], axis]
@@ -199,30 +126,40 @@ def build_boundary_PSLGs(segments, Lx, Ly, Lz):
             holes[i] = np.vstack(holes[i]) if len(holes[i]) else []
         return holes
 
-    def reindex_edges(points, points_ax, edges_ax)
-        edges_flat = edges_ax.flatten()
-        edges_flat_reindexed = np.zeros(
-            edges_ax.shape[0] * edges_ax.shape[1], dtype=np.int64
-        )
-        points_segment = np.empty((points_ax.sum(),3))
-        reindex = {}
-        count = 0
-        for j, vidx in enumerate(edges_flat):
-            try:
-                edges_flat_reindexed[j] = reindex[vidx]
-            except KeyError:
-                points_segment[count] = points[vidx]
-                reindex[vidx] = count
-                edges_flat_reindexed[j] = count
-                count += 1
-        return points_segments, edges_flat_reindexed.reshape(edges_ax.shape)
+    def reindex_edges(points, points_ax, edges_ax):
+        points_segment = points[points_ax]
+        reindex = {old: new for new, old in enumerate(np.where(points_ax)[0])}
+        for i, (v0, v1) in enumerate(edges_ax):
+            edges_ax[i] = np.array([reindex[v0], reindex[v1]])
+        return points_segment, edges_ax
+
+    def build_perim_edge_list(points_ax, perim_refined):
+        # Need to adjust edge indices for perimeter segments
+        v_count = len(points_ax)
+        perim_edges = 4 * [None]
+        for j in range(4):
+            v_count_perim = len(perim_refined[j])
+            perim_vidx = np.arange(v_count, v_count + v_count_perim)
+            mask = np.full(v_count_perim, True)
+            v_count_new = 0
+            for i, p in enumerate(perim_refined[j]):
+                vidx = np.where(np.isclose(npl.norm(points_ax - p, axis=1), 0.))[0]
+                if len(vidx):
+                    mask[i] = False
+                    perim_vidx[i] = vidx[0]
+                else:
+                    perim_vidx[i] = v_count + v_count_new
+                    v_count_new += 1
+            perim_edges[j] = np.array([
+                [perim_vidx[k], perim_vidx[k+1]] for k in range(v_count_perim-1)
+            ])
+            perim_refined[j] = perim_refined[j][mask]
+            v_count += v_count_new
+        return perim_edges
 
     L = np.array([Lx, Ly, Lz])
 
-    # TODO : refactor to support multiple sphere segments
-    points, tris  = segments[0]
-
-    edges = build_edge_list(tris, points)
+    points, edges = compile_points_edges(segments)
 
     # TODO : get target point separation from segment properties.
     #      : For now, get this from mean edge length.
@@ -239,66 +176,62 @@ def build_boundary_PSLGs(segments, Lx, Ly, Lz):
     for i in range(3):
         points[(points_ax[i], i)] = 0.
 
+    # reindex edge vertices
+    points_segments, edges_ax = zip(*[
+        reindex_edges(points, points_ax[i], edges_ax[i]) for i in range(3)
+        # reindex_edges_old(points, points_ax[i], edges_ax[i]) for i in range(3)
+    ])
+
     perim = perim_refined = 3 * [4 * [None]]
-    perim_segs = np.array([[0, 1], [1, 2], [2, 3], [3, 4]])
+    perim_segs = np.array([[0, 1], [1, 2], [2, 3], [3, 0]])
 
     for j in range(3):
+        # Rotate coordinate system by cyclic permutation of axes
+        points_segments[j][:,(0,1,2)] = points_segments[j][:,(j,(j+1)%3,(j+2)%3)]
+
         corners = np.array([
             [0., 0., 0.], [0., L[1], 0.], [0., L[1], L[2]], [0., 0., L[2]]
         ])
 
         points_on_perim = 4 * [None]
-        points_on_perim[0] = np.isclose(points[points_ax[j], 2], 0.)
-        points_on_perim[1] = np.isclose(points[points_ax[j], 1], L[1])
-        points_on_perim[2] = np.isclose(points[points_ax[j], 2], L[2])
-        points_on_perim[3] = np.isclose(points[points_ax[j], 1], 0.)
+        points_on_perim[0] = np.isclose(points_segments[j][:, 2], 0.)
+        points_on_perim[1] = np.isclose(points_segments[j][:, 1], L[1])
+        points_on_perim[2] = np.isclose(points_segments[j][:, 2], L[2])
+        points_on_perim[3] = np.isclose(points_segments[j][:, 1], 0.)
 
         for i in range(2 + 2 * int(not WITH_PBC)):
             axis = 1 + i % 2
             perim[j][i] = np.vstack(
-                (corners[perim_segs[i]], points[points_ax[j]][points_on_perim[i]])
+                (corners[perim_segs[i]], points_segments[j][points_on_perim[i]])
             )
             if WITH_PBC:
                 translate = np.array([0., 0., -L[2]]) if axis == 1\
                     else np.array([0., L[1], 0.])
-                translated_points = points[points_ax[j]][points_on_perim[i + 2]] + translate
+                translated_points = points_segments[j][points_on_perim[i + 2]] + translate
                 perim[j][i] = np.vstack((perim[j][i], translated_points))
             perim[j][i] = perim[j][i][perim[j][i][:, axis].argsort()]
             perim_refined[j][i] = refined_perimeter(perim[j][i], axis)
             if WITH_PBC:
                 perim[j][i+2] = perim[j][i] - translate
                 perim_refined[j][i+2] = perim_refined[j][i] - translate
-        # Rotate coordinate system by cyclic permutation of axes
-        points = points[:, (1, 2, 0)]
+
+        # Put coordinates back in proper order for this axis
+        points_segments[j][:,(j,(j+1)%3,(j+2)%3)] = points_segments[j][:,(0,1,2)]
+
         L = L[np.newaxis, (1, 2, 0)][0]
 
     # TODO : refactor so boundary PSLG is built during above loop avoiding subsequent loops
-    # reindex edge vertices
-    # TODO : There must be a better way of reindexing the edge vertices that this...
-    points_segments, edges_ax = zip(*[
-        reindex_edges(points, points_ax[i], edges_ax[i]) for i in range(3)
-    ])
-
 
     # Build lists of perimeter edges
-    perim_edges = 3 * [4 * [None]]
-    for i in range(3):
-        # Need to adjust edge indices for perimeter segments
-        v_count = points_ax[i].sum()
-        # v_count = points.shape[0]
-        for j in range(4):
-            npoints_perim = perim_refined[i][j].shape[0]
-            perim_edges[i][j] = np.array([[k, k+1] for k in range(npoints_perim-1)])
-            # TODO : should be modifying vertex indices by running vertex count!
-            perim_edges[i][j] += v_count
-            v_count += npoints_perim
+    perim_edges = [
+        build_perim_edge_list(points_segments[i], perim_refined[i])
+        for i in range(3)
+    ]
 
     # add holes
     pslg_holes = add_holes(segments)
 
     # Group together segment and perimeter points and edges for each axis
-    # TODO : vertex indices for sphere vertices in edge array need to be
-    #      : reindexed as a reduced set is passed to triangle.
     boundary_pslgs = []
     for i in range(3):
         pslg_points = np.vstack((
@@ -316,7 +249,7 @@ def triangulate_PSLGs(pslgs):
         # Set mesh info for triangulation
         mesh_data = triangle.MeshInfo()
         mesh_data.set_points(points)
-        mesh_data.set_facets(edges)
+        mesh_data.set_facets(edges.tolist())
         if len(holes):
             mesh_data.set_holes(holes)
 
@@ -337,8 +270,7 @@ def triangulate_PSLGs(pslgs):
         points = np.column_stack((np.zeros(len(mesh.points)), np.array(mesh.points)))
         points = points[:,(-i%3,(1-i)%3,(2-i)%3)]
         tris = np.array(mesh.elements)
-        # TODO : If holes are not required after triangulating boundaries then remove
-        holes = np.column_stack((np.zeros(len(holes)), holes))
+        holes = np.column_stack((np.zeros(len(mesh.holes)), np.array(mesh.holes)))
         holes = holes[:,(-i%3,(1-i)%3,(2-i)%3)]
 
         triangulated_boundaries.append((points, tris, holes))
@@ -351,7 +283,7 @@ def build_tet_mesh(segments, boundaries, Lx, Ly, Lz):
         L = np.array([Lx, Ly, Lz])
         upper_boundaries = []
         for i, (points, tris, holes) in enumerate(lower_boundaries):
-            translate = np.array([L[j] if j==i else 0. for j in range(3)])
+            translate = np.array([[L[j] if j==i else 0. for j in range(3)]])
             points_upper = points.copy() + translate
             tris_upper = tris.copy()
             holes_upper = holes.copy() + translate
@@ -379,7 +311,7 @@ def build_tet_mesh(segments, boundaries, Lx, Ly, Lz):
             np.full(len(all_facets[2]), 5), np.full(len(all_facets[5]), 6),
         ]
         fcount = 7
-        for _, tris in segments[:1]:
+        for _, tris in segments:
             all_facets.append(tris)
             all_markers.append(np.full(len(tris), fcount))
             fcount += 1
@@ -393,39 +325,42 @@ def build_tet_mesh(segments, boundaries, Lx, Ly, Lz):
         return np.vstack(all_holes)
 
     boundaries = duplicate_lower_boundaries(boundaries, Lx, Ly, Lz)
-    pprint(segments)
-    pprint(boundaries)
 
     points = build_point_list(segments, boundaries)
-    pprint(segments)
-    pprint(boundaries)
+    # Fix boundary points to exactly zero
+    for i in range(3):
+        points[(np.isclose(points[:,i], 0.), i)] = 0.
+
     facets, markers = build_facet_list(segments, boundaries)
     holes = build_hole_list(segments)
+
+    output_mesh_poly(points, facets, markers, holes)
 
     rad_edge = 1.4
     min_angle = 18.
     max_volume = None  # 0.00001
-    #options = tet.Options('pq{}/{}Y'.format(rad_edge, min_angle))
-    options = tet.Options('pq')
+    # TODO : Don't mix and match between setting options with argument string
+    #      : and option class attributes. Pick one and be consistent.
+    options = tet.Options('pq{}/{}Y'.format(rad_edge, min_angle))
+    options.docheck = 1
+    options.verbose = 1
 
     mesh = tet.MeshInfo()
     mesh.set_points(points)
-    #mesh.set_facets(facets)
-    mesh.set_facets(facets, markers=markers.tolist())
-    #mesh.set_holes(holes)
+    mesh.set_facets(facets.tolist(), markers=markers.tolist())
+    mesh.set_holes(holes)
 
     return tet.build(mesh, options=options, max_volume=max_volume)
-    # TODO : Improve/address conversion from meshpy data types to numpy
-    # mesh = tet.build(mesh, options=options, max_volume=max_volume)
-    # mesh.points = np.array(mesh.points)
 
-if __name__ == '__main__':
+
+def main():
     Lx, Ly, Lz, prefix = get_args(sys.argv)
     segments = load_segments_geomview(prefix)
     boundary_pslgs = build_boundary_PSLGs(segments, Lx, Ly, Lz)
-    # plot_points_edges(*boundary_pslgs[1][:2])
-    output_boundaries_poly(boundary_pslgs)
     boundaries = triangulate_PSLGs(boundary_pslgs)
-    output_boundaries_geomview(boundaries)
     mesh = build_tet_mesh(segments, boundaries, Lx, Ly, Lz)
-    #mesh.write_vtk('mesh.vtk')
+    mesh.write_vtk('mesh')
+
+
+if __name__ == '__main__':
+    main()
