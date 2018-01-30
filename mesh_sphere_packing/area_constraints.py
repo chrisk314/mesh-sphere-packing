b98e2a716d4ce94446b2241cbdeb932043b96f01
Chris Knight <chrisk314@gmail.com>
2018-01-30 16:31:55 +0000

    Adds area refinement logic (#10)

--- mesh_sphere_packing/area_constraints.py
+++ mesh_sphere_packing/area_constraints.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from numpy import linalg as npl
 
 from mesh_sphere_packing import ONE_THIRD, GROWTH_LIMIT
 
@@ -9,8 +10,8 @@ class AreaConstraints(object):
     """Constructs grid of area constraints for triangulation of domain boundaries.
     """
 
-    cutoff = 0.5        # Distance beyond which particles do not force added points
-    cell_width = 2.     # Width of grid cells in units of characteristic length, ds
+    cutoff = 0.3        # Distance beyond which particles do not force added points
+    cell_width = 1.     # Width of grid cells in units of characteristic length, ds
 
     def __init__(self, args, ds):
         # TODO : cutoff distance is dependent on mesh resolution and should exist
@@ -53,17 +54,27 @@ class AreaConstraints(object):
         #      : to axis as they will have geometry at both sides of domain.
         return particles
 
-    def area_constraint(self, x, y):
+    def area_constraint(self, x, y, p_xy, elevation, rad):
         """Return value for area constraint factor at coordinates x, y based
         on particle positions.
         """
         # TODO : populate the area constraint grid with interpolated values of
         #      : some sizing function, which depends on the particle positions,
         #      : f(cx,cy) -> R, where cx and cy are the triangle center coordinates.
-        growth = GROWTH_LIMIT
-        return self.dA * growth
 
-    def constraint_grid(self, particles, axis):
+        delta = npl.norm(p_xy - np.array([x, y]), axis=1)
+        nbrs = np.where(delta < rad)[0]
+        if not len(nbrs):
+            growth = GROWTH_LIMIT
+        else:
+            # TODO : This requires some tuning to get the desired refinement.
+            g_min_part = 1. + (elevation / self.cutoff)**2. * (GROWTH_LIMIT - 1.)
+            g_min_part *= 0.8
+            decay = (delta / rad)**2.5
+            growth = np.min(g_min_part * (1. - decay) + GROWTH_LIMIT * decay)
+        return growth * self.dA
+
+    def constraint_grid(self, axis):
         width = self.cell_width * self.ds
         Lx, Ly = self.L[(axis+1)%3], self.L[(axis+2)%3]
         nx, ny = int(Lx / width), int(Ly / width)  # number of cells in the grid
@@ -73,7 +84,15 @@ class AreaConstraints(object):
         x = np.arange(0.5 * dx, Lx, dx)
         y = np.arange(0.5 * dy, Ly, dy)
         # TODO : Improve variable naming here.
-        return [[self.area_constraint(_x, _y) for _x in x] for _y in y]
+
+        rad = self.particles[axis][:,3]
+        elevation = self.particles[axis][:,axis] - rad
+        p_xy = self.particles[axis][:,((axis+1)%3,(axis+2)%3)]
+
+        return [
+            [self.area_constraint(_x, _y, p_xy, elevation, rad) for _x in x]
+            for _y in y
+        ]
 
     def build_area_constraint_grid(self, args):
         # TODO : Change this to use particle data read from file. For now mocking
@@ -92,6 +111,5 @@ class AreaConstraints(object):
             self.duplicate_edge_particles(p, axis)
             for axis, p in enumerate(p_ax)
         ]
-        self.grid = [
-            self.constraint_grid(p, axis) for axis, p in enumerate(p_ax)
-        ]
+        self.particles = p_ax
+        self.grid = [self.constraint_grid(axis) for axis in range(3)]
