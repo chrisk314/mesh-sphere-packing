6220a0dd953344037aff93a6f7b7e79feaa99b6b
Chris Knight <chrisk314@gmail.com>
2018-01-31 14:22:15 +0000

    Moves all parsing and data loading to single module

--- mesh_sphere_packing/build.py
+++ mesh_sphere_packing/build.py
@@ -1,24 +1,18 @@
 
-from mesh_sphere_packing.parser import parser
-from mesh_sphere_packing.reader import read_config_file, read_particle_file
+from mesh_sphere_packing.parse import get_parser, load_data
 from mesh_sphere_packing.splitsphere import splitsphere
 from mesh_sphere_packing.boundarypslg import boundarypslg
 from mesh_sphere_packing.tetmesh import build_tetmesh
 
 
 def build(args):
-    config = read_config_file(args.config_file)
-    particle_file = args.particle_file or config.particle_file
-    if particle_file:
-        L, PBC, particles = read_particle_file(particle_file)
-
     segments = splitsphere(args)
     boundaries = boundarypslg(segments, args)
     mesh = build_tetmesh(segments, boundaries, args)
-
     mesh.write_vtk('mesh.vtk')
 
 
 if __name__ == '__main__':
-    args = parser.parse_args()
+    args = get_parser().parse_args()
+    data = load_data(args)
     build(args)
--- mesh_sphere_packing/parse.py
+++ mesh_sphere_packing/parse.py
@@ -1,7 +1,44 @@
 
+import argparse
+from argparse import FileType, RawTextHelpFormatter
+from collections import namedtuple
+
 import numpy as np
 import yaml
-from collections import namedtuple
+
+from mesh_sphere_packing.splitsphere import Domain
+
+DESC = """Generates tetrahedral meshes of the interstitial spaces in packings of spheres
+for finite volume simulations.
+
+-p, --particle-file option expects an ascii encoded text file specifying domain
+dimensions, periodic boundary flags, and particle center coordinates and radii
+in the following format:
+
+    line 1      : <domain_extent_x> <domain_extent_y> <domain_extent_z>
+    line 2      : <x_bound_periodic> <y_bound_periodic> <z_bound_periodic>
+    line 3      : <p1_id> <p1_x> <p1_y> <p1_z> <p1_radius>
+    ...
+    line N+2    : <pN_id> <pN_x> <pN_y> <pN_z> <pN_radius>
+
+Here is an example particle data file:
+
+    $ cat my_particle_file.txt
+    2.0e-03 2.0e-3 2.0e-3
+    0 1 1
+    0 9.10502e-4 7.73356e-4 1.66188e-3 5.08645e-4
+    1 3.83301e-4 9.34604e-4 6.5982se-6 5.50847e-4
+    ...
+    N 1.51262e-3 2.95234e-4 5.63176e-4 2.08111e-4
+
+Particles need not be specified in order of id or any other order. The ids are
+used to specify boundary conditions on the particle surfaces.
+
+"""
+
+
+class DataLoaderError(Exception):
+    pass
 
 
 class ParticleFileReaderError(Exception):
@@ -10,6 +47,70 @@ class ParticleFileReaderError(Exception):
 
 class ConfigFileReaderError(Exception):
     pass
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=DESC, formatter_class=RawTextHelpFormatter
+    )
+    parser.add_argument(
+        '-V', '--version', action='version', version='0.1'
+    )
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='output detailed information'
+    )
+    parser.add_argument(
+        '-q', '--quiet', action='store_true', help='suppress all output'
+    )
+    parser.add_argument(
+        '-cx', '--particle-center', nargs=3, type=float, required=False,
+        metavar='X', help='list of particle center coordinates',
+    )
+    parser.add_argument(
+        '-r', '--particle-radius', type=float, required=False,
+        metavar='R', help='particle radius',
+    )
+    parser.add_argument(
+        '-l', '--domain-dimensions', nargs=3, type=float, required=False,
+        metavar='L', help='list of domain side lengths',
+    )
+    parser.add_argument(
+        '--pbc', nargs=3, type=int, required=False,
+        metavar='0|1', help='list of periodic boundary flags',
+    )
+    parser.add_argument(
+        '-p', '--particle-file', type=FileType(mode='r'), required=False, metavar='FILE',
+        help='path to file specifying particle data (see main description)'
+    )
+    parser.add_argument(
+        '-c', '--config-file', type=FileType(mode='r'), required=False, metavar='FILE',
+        help='path to yaml config file'
+    )
+    parser.set_defaults(func=load_data)
+    return parser
+
+
+def load_data(args):
+    config = read_config_file(args.config_file)
+    particle_file = args.particle_file or config.particle_file
+    if particle_file:
+        L, PBC, particles = read_particle_file(particle_file)
+    else:
+        single_mode_missing_required = [
+            not args.particle_center,
+            not args.particle_radius,
+            not args.domain_dimensions,
+            not args.pbc,
+        ]
+        if any(single_mode_missing_required):
+            raise DataLoaderError('Insufficient data provided to build mesh.')
+        L = args.domain_dimensions
+        PBC = args.pbc
+        particles = np.array([
+            [0] + args.particle_center + [args.particle_radius]
+        ])
+    domain = Domain(L, PBC)
+    return domain, particles, config
 
 
 def read_particle_file(pfile):
