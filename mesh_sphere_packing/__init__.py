1bde5db5d531b4a290ee7773783481959d110244
Chris Knight <chrisk314@gmail.com>
2018-01-31 12:16:08 +0000

    Moves constants to top level

--- mesh_sphere_packing/__init__.py
+++ mesh_sphere_packing/__init__.py
@@ -1,3 +1,4 @@
 
-ONE_THIRD = 0.3333333333333333
-GROWTH_LIMIT = 5
+TOL = 1.e-10                        # Floating point tolerance
+ONE_THIRD = 0.3333333333333333      # One third: nothing more, nothing less.
+GROWTH_LIMIT = 5                    # Limits area of boundary triangles
