cf11249616f789cd10e455490b4d2faa44c2a8fc
Chris Knight <chrisk314@gmail.com>
2018-01-31 11:28:47 +0000

    Updates test mock object

--- mesh_sphere_packing/tests/test_build.py
+++ mesh_sphere_packing/tests/test_build.py
@@ -6,7 +6,9 @@ from mesh_sphere_packing.build import build
 args = Mock(
     particle_center=[0., 0., 0.],
     particle_radius=.5,
-    domain_dimensions=[2., 2., 2.]
+    domain_dimensions=[2., 2., 2.],
+    config_file=None,
+    particle_file=None,
 )
 
 
