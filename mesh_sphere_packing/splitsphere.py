93c4552c3577ac091848c4e9bbd53506053be7e7
Chris Knight <chrisk314@gmail.com>
2018-02-02 16:51:09 +0000

    Implements generic sphere splitting algorithm (#4)

--- mesh_sphere_packing/splitsphere.py
+++ mesh_sphere_packing/splitsphere.py
@@ -7,6 +7,34 @@ from scipy.spatial.qhull import ConvexHull
 from mesh_sphere_packing import TOL, ONE_THIRD
 
 
+def flatten(l):
+    """Return flattened list."""
+    flat = []
+    for x in l:
+        if isinstance(x, list):
+            flat += flatten(x)
+        else:
+            flat += [x]
+    return flat
+
+
+def reindex_tris(points, tris):
+    count = 0
+    reindex = {}
+    points_reindexed = np.empty(points.shape)
+    for t in tris:
+        for idx in t:
+            try:
+                reindex[idx]
+            except KeyError:
+                reindex[idx] = count
+                points_reindexed[count] = points[idx]
+                count += 1
+    points = points_reindexed[:count]
+    tris = np.array([[reindex[idx] for idx in t] for t in tris])
+    return points, tris
+
+
 class Domain(object):
 
     """Spatial cuboid shaped domain in R^3."""
@@ -27,6 +55,13 @@ class Sphere(object):
         self.r = r
         self.points = None
 
+    def initialise_points(self, num_points=200):
+        num_points = 50
+        self.gen_spiral_points(num_points=num_points)
+        self.filter_points()
+        self.min = self.points.min(axis=0)
+        self.max = self.points.max(axis=0)
+
     def gen_spiral_points(self, num_points=200):
         indices = np.arange(0, num_points, dtype=float) + 0.5
 
@@ -37,8 +72,6 @@ class Sphere(object):
         self.points[:,0] = self.x[0] + self.r * (np.cos(theta) * np.sin(phi))
         self.points[:,1] = self.x[1] + self.r * (np.sin(theta) * np.sin(phi))
         self.points[:,2] = self.x[2] + self.r * np.cos(phi)
-
-        self.filter_points()
 
     def filter_points(self, strength=0.15):
         """Return array of filtered points excluding points lying too close
@@ -62,160 +95,149 @@ class Sphere(object):
 
         self.points = self.points[~close_x & ~close_y & ~close_z]
 
+    def split_axis_recursive(self, points, axis, trans):
+        """Return nested list of points sets resulting from recursive splitting
+        of sphere along the three coordinate axes.
+        """
+        cross_low = self.min[axis] < 0.
+        cross_high = self.max[axis] > self.domain.L[axis]
+        if cross_low or cross_high:
+            self.split_axis[axis] = True
+            trans_in, trans_out = trans.copy(), trans.copy()
+            if cross_low:
+                out = points[:, axis] < 0.
+                trans_out[axis] = 1.
+            elif self.max[axis] > self.domain.L[axis]:
+                out = self.points[:, axis] > self.domain.L
+                trans_out[axis] = -1.
+                self.bound_high[axis] = True
+            if axis < 2:
+                return [
+                    self.split_axis_recursive(points[~out], axis+1, trans_in),
+                    self.split_axis_recursive(points[out], axis+1, trans_out)
+                ]
+            return [(points[~out], trans_in), (points[out], trans_out)]
+        if axis < 2:
+            return self.split_axis_recursive(points, axis+1, trans)
+        return points, trans
+
     def split(self, domain):
+
+        def curve_intersection_points(c, r, i, i1, i2, L):
+            # Check for splits of this curve along axis i1
+            ci_points = np.full((2,3), c[i])
+            cnt = 0
+            # If the curve just touches a bound along i1 add a single point...
+            if np.isclose(c[i1] + r, L[i1]):    # ...on the upper bound...
+                ci_points[cnt,i1] = L[i1]
+                ci_points[cnt,i2] = c[i2]
+                cnt += 1
+            elif np.isclose(c[i1] - r, 0.):     # ...or on the lower bound.
+                ci_points[cnt,i1] = 0.
+                ci_points[cnt,i2] = c[i2]
+                cnt += 1
+            # Otherwise, if the sphere is split along i1 the curve may cross the bounds.
+            elif self.split_axis[i1]:
+                if c[i1] + r > L[i1]:   # Add two points at upper bound along i1...
+                    ci_points[cnt:cnt+1,i1] = L[i1]
+                elif c[i1] - r < 0.:    # ...or add two points at lower bound along i1...
+                    ci_points[cnt:cnt+1,i1] = 0.
+                else:                   # ...or add no points at bounds along i1.
+                    return ci_points[:cnt]
+                di1 = ci_points[cnt,i1] - c[i1]
+                di2 = np.sqrt(r**2 - di1**2)
+                ci_points[cnt,i2] = c[i2] + di2
+                ci_points[cnt+1,i2] = c[i2] - di2
+                cnt += 2
+            return ci_points[:cnt]
+
+        def get_add_phi_points(phi1, phi2, r, cx, i, ds):
+            i1, i2 = (i+1)%3, (i+2)%3
+            phi_disp = phi2 - phi1
+            n_add = int(np.ceil(phi_disp * r / ds))
+            phi = phi1 + (phi_disp / n_add) * np.arange(n_add+1)
+            points = np.empty((len(phi),3))
+            points[:,i] = cx[i]
+            points[:,i1] = cx[i1] + r * np.cos(phi)
+            points[:,i2] = cx[i2] + r * np.sin(phi)
+            return points
+
+        # Construct analytical intersection curves of sphere on boundaries
+        def iloop_zones(points, axis, L):
+            com = points.mean(axis=0)
+            z = ~((0. < com) & (com < L))
+            z[axis] = False
+            z1 = 1*z[2] + 2*z[1] + 4*z[0]
+            z[axis] = True
+            z2 = 1*z[2] + 2*z[1] + 4*z[0]
+            return (z1, z2)
 
         self.domain = domain
 
         if not self.points:
-            self.gen_spiral_points()
+            self.initialise_points()
 
-        # first split along x
-        out_x = self.points[:, 0] < 0.
-        out_y = self.points[:, 1] < 0.
-        out_z = self.points[:, 2] < 0.
-
-        # TODO : This is a quick and dirty fix to allow points for particles
-        #      : inside the domain to be added without issue. Ultimately these
-        #      : cases needs to be handled in a more generally, comprehensively.
-        if not np.any([out_x, out_y, out_z]):
-            # TODO : numpy array is returned here but this function returns
-            #      : SpherePiece objects elsewhere!
-            return [self.points]
-
-        sphere_pieces = [
-            self.points[~out_x & ~out_y & ~out_z],
-            self.points[ out_x & ~out_y & ~out_z],
-            self.points[~out_x &  out_y & ~out_z],
-            self.points[ out_x &  out_y & ~out_z],
-            self.points[~out_x & ~out_y &  out_z],
-            self.points[ out_x & ~out_y &  out_z],
-            self.points[~out_x &  out_y &  out_z],
-            self.points[ out_x &  out_y &  out_z],
-        ]
-
-        # TODO : refactor this huge procedural beast of a function
-
-        def get_add_phi_points(phi_1, phi_2, r, ds, fn_x, fn_y, fn_z):
-            phi_disp = phi_2 - phi_1
-            n_add = int(np.ceil(phi_disp * r / ds))
-            d_phi = phi_disp / n_add
-            phi_i = phi_1 + d_phi * np.arange(n_add+1)
-            # return np.column_stack((fn_x(phi_i), fn_y(phi_i), fn_z(phi_i)))
-            # TODO : Only guaranteed to require extra point at origin for sphere
-            #      : crossing all three boundaries
-            return np.concatenate((
-                np.zeros((1,3)),
-                np.column_stack((fn_x(phi_i), fn_y(phi_i), fn_z(phi_i)))
-            ))
-
-        r_inv = 1. / self.r
+        # Inverse of point density per unit length (required distance between points).
         ds = (len(self.points) / (4. * np.pi * self.r**2.))**-0.5
 
-        # Curves on x boundary (y-z plane) -------------------------------------------------------------
-        fn_x = lambda phi: np.full(len(phi), 0.)
-        fn_y = lambda phi: self.x[1] + self.r * np.cos(phi)
-        fn_z = lambda phi: self.x[2] + self.r * np.sin(phi)
+        self.split_axis = np.full(3, False)  # True if the particle is split along axis
+        self.bound_high = np.full(3, False)  # True/false if particle crosses low/high bound
 
-        # curve 1
-        phi_1 = np.arcsin(-self.x[2] * r_inv)
-        phi_2 = 0.5 * np.pi + np.arcsin(self.x[1] * r_inv)
-        curve_points = get_add_phi_points(phi_1, phi_2, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[0] = np.concatenate((sphere_pieces[0], curve_points), axis=0)
-        sphere_pieces[1] = np.concatenate((sphere_pieces[1], curve_points), axis=0)
+        # Partition points of sphere into regions either side of boundaries
+        sphere_pieces = flatten([
+            self.split_axis_recursive(self.points, 0, np.zeros(3, dtype=np.float64))
+        ])
 
-        # curve 2
-        phi_3 = np.pi - phi_1
-        curve_points = get_add_phi_points(phi_2, phi_3, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[2] = np.concatenate((sphere_pieces[2], curve_points), axis=0)
-        sphere_pieces[3] = np.concatenate((sphere_pieces[3], curve_points), axis=0)
+        if not np.any(self.split_axis):
+            # No splits so return the entire sphere
+            return [SpherePiece(self, self.points, np.zeros(3))]
 
-        # curve 3
-        phi_4 = 2. * np.pi - phi_2
-        curve_points = get_add_phi_points(phi_3, phi_4, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[6] = np.concatenate((sphere_pieces[6], curve_points), axis=0)
-        sphere_pieces[7] = np.concatenate((sphere_pieces[7], curve_points), axis=0)
-
-        # curve 3
-        phi_5 = 2. * np.pi + phi_1
-        curve_points = get_add_phi_points(phi_4, phi_5, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[4] = np.concatenate((sphere_pieces[4], curve_points), axis=0)
-        sphere_pieces[5] = np.concatenate((sphere_pieces[5], curve_points), axis=0)
-
-        # Curves on y boundary (x-z plane) -------------------------------------------------------------
-        fn_x = lambda phi: self.x[0] + self.r * np.sin(phi)
-        fn_y = lambda phi: np.full(len(phi), 0.)
-        fn_z = lambda phi: self.x[2] + self.r * np.cos(phi)
-
-        # curve 1
-        phi_1 = np.arcsin(-self.x[0] * r_inv)
-        phi_2 = 0.5 * np.pi + np.arcsin(self.x[2] * r_inv)
-        curve_points = get_add_phi_points(phi_1, phi_2, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[0] = np.concatenate((sphere_pieces[0], curve_points), axis=0)
-        sphere_pieces[2] = np.concatenate((sphere_pieces[2], curve_points), axis=0)
-
-        # curve 2
-        phi_3 = np.pi - phi_1
-        curve_points = get_add_phi_points(phi_2, phi_3, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[4] = np.concatenate((sphere_pieces[4], curve_points), axis=0)
-        sphere_pieces[6] = np.concatenate((sphere_pieces[6], curve_points), axis=0)
-
-        # curve 3
-        phi_4 = 2. * np.pi - phi_2
-        curve_points = get_add_phi_points(phi_3, phi_4, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[5] = np.concatenate((sphere_pieces[5], curve_points), axis=0)
-        sphere_pieces[7] = np.concatenate((sphere_pieces[7], curve_points), axis=0)
-
-        # curve 3
-        phi_5 = 2. * np.pi + phi_1
-        curve_points = get_add_phi_points(phi_4, phi_5, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[1] = np.concatenate((sphere_pieces[1], curve_points), axis=0)
-        sphere_pieces[3] = np.concatenate((sphere_pieces[3], curve_points), axis=0)
-
-        # Curves on z boundary (x-y plane) -------------------------------------------------------------
-        fn_x = lambda phi: self.x[0] + self.r * np.cos(phi)
-        fn_y = lambda phi: self.x[1] + self.r * np.sin(phi)
-        fn_z = lambda phi: np.full(len(phi), 0.)
-
-        # curve 1
-        phi_1 = np.arcsin(-self.x[1] * r_inv)
-        phi_2 = 0.5 * np.pi + np.arcsin(self.x[0] * r_inv)
-        curve_points = get_add_phi_points(phi_1, phi_2, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[0] = np.concatenate((sphere_pieces[0], curve_points), axis=0)
-        sphere_pieces[4] = np.concatenate((sphere_pieces[4], curve_points), axis=0)
-
-        # curve 2
-        phi_3 = np.pi - phi_1
-        curve_points = get_add_phi_points(phi_2, phi_3, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[1] = np.concatenate((sphere_pieces[1], curve_points), axis=0)
-        sphere_pieces[5] = np.concatenate((sphere_pieces[5], curve_points), axis=0)
-
-        # curve 3
-        phi_4 = 2. * np.pi - phi_2
-        curve_points = get_add_phi_points(phi_3, phi_4, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[3] = np.concatenate((sphere_pieces[3], curve_points), axis=0)
-        sphere_pieces[7] = np.concatenate((sphere_pieces[7], curve_points), axis=0)
-
-        # curve 3
-        phi_5 = 2. * np.pi + phi_1
-        curve_points = get_add_phi_points(phi_4, phi_5, self.r, ds, fn_x, fn_y, fn_z)
-        sphere_pieces[2] = np.concatenate((sphere_pieces[2], curve_points), axis=0)
-        sphere_pieces[6] = np.concatenate((sphere_pieces[6], curve_points), axis=0)
-
-        # TODO : Implement logic to generate translations in a general way.
-        translations = [
-            np.array([0., 0., 0.]),
-            np.array([1., 0., 0.]),
-            np.array([0., 1., 0.]),
-            np.array([1., 1., 0.]),
-            np.array([0., 0., 1.]),
-            np.array([1., 0., 1.]),
-            np.array([0., 1., 1.]),
-            np.array([1., 1., 1.]),
+        # Filter out empty pieces
+        sphere_pieces = [
+            (points, trans) for (points, trans) in sphere_pieces if len(points)
         ]
+        sphere_pieces, translations = [list(tup) for tup in zip(*sphere_pieces)]
+        # Construct zone to piece mapping
+        zone_map = {
+            1*bool(t[2]) + 2*bool(t[1]) + 4*bool(t[0]): idx
+            for idx, t in enumerate(translations)
+        }
+
+        ci = []
+        for i in range(3):
+            if self.split_axis[i]:
+                i1, i2 = (i+1)%3, (i+2)%3
+                c = self.x.copy()
+                c[i] = self.bound_high[i] * self.domain.L[i]
+                r = np.sqrt(self.r**2 - (self.x[i] - c[i])**2)
+
+                ci_points = np.vstack((
+                    curve_intersection_points(c, r, i, i1, i2, domain.L),
+                    curve_intersection_points(c, r, i, i2, i1, domain.L)
+                ))
+
+                # Sort points by angle 0 -> 2*pi
+                if len(ci_points):
+                    phi = np.angle(ci_points[:,i1] + 1j * ci_points[:,i2])
+                    phi = np.sort(np.where(phi < 0., 2*np.pi+phi, phi))
+                    phi = np.append(phi, phi[0] + 2 * np.pi)
+                else:
+                    phi = np.array([0., 2 * np.pi])
+
+                # Add points to intersection curve
+                for phi1, phi2 in zip(phi[:-1], phi[1:]):
+                    add_points = get_add_phi_points(phi1, phi2, r, c, i, ds)
+                    z1, z2 = iloop_zones(add_points, i, self.domain.L)
+                    sphere_pieces[zone_map[z1]] = np.vstack((
+                        sphere_pieces[zone_map[z1]], add_points
+                    ))
+                    sphere_pieces[zone_map[z2]] = np.vstack((
+                        sphere_pieces[zone_map[z2]], add_points
+                    ))
 
         return [
-            SpherePiece(self, points, trans) for points, trans
-            in zip(sphere_pieces, translations)
+            SpherePiece(self, points, trans)
+            for points, trans in zip(sphere_pieces, translations)
         ]
 
 
@@ -255,21 +277,6 @@ class SpherePiece(object):
             c_rel = c - sphere_center
             return np.divide(c_rel, npl.norm(c_rel, axis=1)[:, np.newaxis])
 
-        def reindex_tris(tris, points):
-            count = 0
-            reindex = {}
-            points_reindexed = np.empty(points.shape)
-            for t in tris:
-                for idx in t:
-                    try:
-                        reindex[idx]
-                    except KeyError:
-                        reindex[idx] = count
-                        points_reindexed[count] = points[idx]
-                        count += 1
-            self.points = points_reindexed[:count]
-            self.tris = np.array([[reindex[idx] for idx in t] for t in tris])
-
         def extract_surface_tris_from_chull(chull):
             # TODO : Find a cleaner/simpler/more elegant way of extracting sphere triangles.
             #      : Could check if all x, y, or z components are the same - i.e. triangle
@@ -282,9 +289,10 @@ class SpherePiece(object):
             surf_tris = chull.simplices[mask]
             return surf_tris
 
+        #chull = ConvexHull(np.vstack((self.points, np.zeros((1,3)))))
         chull = ConvexHull(self.points)
         surf_tris = extract_surface_tris_from_chull(chull)
-        reindex_tris(surf_tris, chull.points)
+        self.points, self.tris = reindex_tris(chull.points, surf_tris)
 
     def apply_laplacian_smoothing(self):
         # TODO : implement Laplacian smoothing of inner sphere piece vertices
