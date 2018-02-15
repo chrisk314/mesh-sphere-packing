96498c59fc9dfd0449c11fe19686cde21f966106
Chris Knight <chrisk314@gmail.com>
2018-02-15 10:45:26 +0000

    Incrememnts version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='mesh_sphere_packing',
-    version='0.1',
+    version='0.3',
     description='Mesh generator for producing tetrahedral meshes with periodic '\
         'boundaries from packings of spheres.',
     packages=find_packages(),
